4eafdee297f144e943b06552857b2564372874b5
habiwan <habiwan@me.com>
2026-03-29 12:26:01 +0100

    Added fixomp function inside the profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,11 @@
+function fixomp {
+    $profileContent = @(
+        'fastfetch --logo "$HOME\hwk.txt"',
+        'oh-my-posh init pwsh | Invoke-Expression',
+        '# To repair this file again, type: fixomp'
+    )
+    $profileContent | Out-File -FilePath $PROFILE -Encoding utf8
+    Write-Host "Profile repaired! Restart your terminal to see changes." -ForegroundColor Green
+}
 fastfetch --logo "$HOME\hwk.txt"
 oh-my-posh init pwsh | Invoke-Expression
